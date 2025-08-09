8134e1e1788cce58c0b68a0c80f434c638e4dd44
Amirali <amiraliesmaeli741@gmail.com>
2025-08-09 14:14:11 +0330

    Enable CORS for all origins in Django settings

--- core/todo/management/commands/insert_data.py
+++ core/todo/management/commands/insert_data.py
@@ -4,16 +4,19 @@ import random
 from accounts.models import User
 from todo.models import Task
 
+
 class Command(BaseCommand):
     help = "inserting dummy data"
 
     def handle(self, *args, **options):
-      fake = Faker()
-      user = User.objects.create_user(email=fake.email(), password="Test@123456")
+        fake = Faker()
+        user = User.objects.create_user(
+            email=fake.email(), password="Test@123456"
+        )
 
-      for _ in range(5):
-          Task.objects.create(
-            user = user,
-            title = fake.text(max_nb_chars=15),
-            complete = random.choice([True,False]),
-          )
+        for _ in range(5):
+            Task.objects.create(
+                user=user,
+                title=fake.text(max_nb_chars=15),
+                complete=random.choice([True, False]),
+            )
